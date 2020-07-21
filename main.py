e594119fada2cec97e06a6fd89afe07014ae5894
gracejoh <ganwynj@icloud.com>
2020-07-21 15:10:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+list1 = ["cat", "dog", 5]
+for item in list1:
+  print(item)
+
+###
+
+list2 = [1,2,4,6]
+for i in range(len(list1)):
+  list1[i] = 5
+print (list1)
+
+###
+
+list3 = [5, 10]
+list3.append(15)
+print(list3)
+
+###
+
+str1 = "hello my friend"
+list4 = list(str1)
+print(list4)
+
+###
+
